749a90f28b7df295748af6e2947141178893380e
Pang2004 <yanhern00@gmail.com>
2026-08-15 23:10:46 +0800

    Updated initial code to simply greet the user and exit

--- src/main/java/Yanny.java
+++ src/main/java/Yanny.java
@@ -1,5 +1,6 @@
 public class Yanny {
-      public static void main(String[] args) {
+  public static void main(String[] args) {
+      String separator = "____________________________________________________________";
           String banner = "____    ____  ___      .__   __. .__   __. ____    ____\n"
                   + "\\   \\  /   / /   \\     |  \\ |  | |  \\ |  | \\   \\  /   /\n"
                   + " \\   \\/   / /  ^  \\    |   \\|  | |   \\|  |  \\   \\/   /\n"
@@ -7,6 +8,13 @@ public class Yanny {
                   + "    |  |  /  _____  \\  |  |\\   | |  |\\   |     |  |\n"
                   + "    |__| /__/     \\__\\ |__| \\__| |__| \\__|     |__|";
 
+          System.out.println(separator);
           System.out.println(banner);
+          System.out.println("");
+          System.out.println("Hello! I'm Yanny.");
+          System.out.println("What can I do for you?");
+          System.out.println(separator);
+          System.out.println("Bye. Hope to see you again soon!");
+          System.out.println(separator);
       }
   }
